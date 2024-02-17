9c45ffbb90ae02fee44bdf813b48513226440b8d
ZarikZa <schemetov.ya@yandex.ru>
2024-02-17 13:47:03 +0300

    postavte 5

---
+++ practa3.cpp
@@ -0,0 +1,30 @@
+﻿// practa3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    const int dlina = 3;
+    double sum{};
+    const int shirina = 5;
+    double matrix[dlina][shirina];
+    int colvo{};
+    double cred;
+    cout << "Вводите 15 чисел: ";
+    for (int i = 0; i < dlina; i++)
+    {
+        for (int j = 0; j < shirina; j++)
+        {
+            cin >> matrix[i][j];
+            sum += matrix[i][j];
+            colvo++;
+        }
+    }
+    cred = sum / colvo;
+    cout << "Среднее арифметическое матрицы: ";
+    cout << cred;
+}
